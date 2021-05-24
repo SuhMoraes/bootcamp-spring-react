5e56dfe0e17920aa75a2c478733fdfec17d0d473
Suh Moraes <suh.alvesmoraes@gmai.com>
2021-05-24 09:00:40 -0300

    Update Category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -8,11 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
@@ -31,7 +31,7 @@ public class CategoryResource {
 		
 	}
 		
-		@GetMapping(value = "/{id}")
+		@GetMapping(value= "/{id}")
 		public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
 			CategoryDTO dto = service.findById(id);		
 			return ResponseEntity.ok().body(dto);
@@ -46,11 +46,14 @@ public class CategoryResource {
 			return ResponseEntity.created(uri).body(dto);
 			
 		}
-
-		private Object buildAnExpand(Long id) {
-			// TODO Auto-generated method stub
-			return null;
+		
+		@PutMapping(value= "/{id}")
+		public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+			dto = service.update(id, dto);
+			return ResponseEntity.ok().body(dto);
 		}
+
+		
 		
 
 }
